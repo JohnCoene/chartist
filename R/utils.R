455a9c374fd91ce6b84dd52b3f6da398cee1f9da
John Coene <jcoenep@gmail.com>
2017-02-22 12:19:01 +0800

    utils cleanup

--- R/utils.R
+++ R/utils.R
@@ -17,23 +17,21 @@ sumJS <- function(serie) {
 }
 
 anim_fun <- function(p, type, target, d){
-  
+
   if(length(p$x$anim$FUN)){
-    
+
     foo <- substr(p$x$anim$FUN, 0, nchar(p$x$anim$FUN)-1)
-    
+
     d <- paste0(foo, "
       if(", type, "){
         data.element.animate({", target, ":{", d, "}});
       }
     }")
-    
+
     fun <- htmlwidgets::JS(d)
-    
+
     opts <- list(FUN = fun)
-    
-    p$x$anim <- opts
-    
+
   } else {
     d <- paste0("
     function(data){
@@ -41,25 +39,26 @@ anim_fun <- function(p, type, target, d){
         data.element.animate({", target, ":{", d, "}});
       }
     }")
-    
+
     fun <- htmlwidgets::JS(d)
-    
+
     opts <- list(FUN = fun)
-    
-    p$x$anim <- opts
+
   }
-  
+
+  p$x$anim <- opts
+
   p
-  
+
 }
 
 type_it <- function(type){
-  
+
   if(length(type) > 1){
     type <- paste0(type, collapse = "' || data.type === '")
-  } 
-  
+  }
+
   type <- paste0("data.type === '", type, "'")
-  
+
   return(type)
 }
